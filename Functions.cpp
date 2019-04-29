b64081ffe7b7cee5d1b2105cdc2ce16c827d72a5
habeeba-yasser <50086611+habeeba-yasser@users.noreply.github.com>
2019-04-29 14:49:06 +0200

    Update Functions.cpp

--- Functions.cpp
+++ Functions.cpp
@@ -3,10 +3,58 @@
 #include <SFML/Graphics.hpp>
 #include <cstdlib>
 #include <ctime>
-
+#include <fstream>
+#include <iostream>
+#include <string>
 using namespace std;
 using namespace sf;
-
+void Map_Parser(ifstream &file,Block* Map [8] [8]){
+    string str[9];
+    file.open("level1.txt");
+    if (!file.is_open()){
+        cout<<"Unable to open file";
+    }
+    for(int i=0;i<9;i++){
+        getline(file, str[i]);
+    }
+    int counter=stoi(str[8]);
+    file.close();
+    for(int i=0;i<8;i++){
+        for(int j=0;j<8;j++){
+            if(str[i][j]=='#'){
+                Map[i][j]= new WallBlock;
+                Map[i][j]->setPosition(j*50+100, i*50+100);
+            }
+            else if(str[i][j]=='!'){
+                Map[i][j]= new TargetBlock;
+                Map[i][j]->setPosition(j*50+100, i*50+100);
+            }
+            else if(str[i][j]=='?'){
+                Map[i][j]= new MovingBlock;
+                Map[i][j]->setPosition(j*50+100, i*50+100);
+            }
+            else if(str[i][j]=='@'){
+                Map[i][j]= new SandBlock;
+                Map[i][j]->setPosition(j*50+100, i*50+100);
+            }
+            else if(str[i][j]=='/'){
+                Map[i][j]= new EmptyBlock;
+                Map[i][j]->setPosition(j*50+100, i*50+100);
+            }
+           /* else if(str[i][j]=='$'){
+                Map[i][j]= new CurrentPlayer;
+                Map[i][j]->setPosition(j*50+100, i*50+100);
+            }*/
+            else if(str[i][j]=='+'){
+                Map[i][j]= new AddLives;
+                Map[i][j]->setPosition(j*50+100, i*50+100);
+            }
+            else if(str[i][j]=='x'){
+                Map[i][j]= new AddMoves;
+                Map[i][j]->setPosition(j*50+100, i*50+100);
+            }
+    }
+}
 void Play_Sound(int index)
 {
     SoundBuffer Buffer;
@@ -37,7 +85,7 @@ void Display_Splash(RenderWindow& window)
     while(clock.getElapsedTime()<seconds(5))
     { };
 }
-void Change_Theme(int index, Block* Map [9] [9])
+void Change_Theme(int index, Block* Map [8] [8])
 {
     for (int i = 0; i<9; i++)
     {
