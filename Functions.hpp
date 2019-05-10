653921972721b55b5582e924896d7f9ae857dadf
habeeba-yasser <50086611+habeeba-yasser@users.noreply.github.com>
2019-05-10 22:07:05 +0200

    Update Functions.hpp

--- Functions.hpp
+++ Functions.hpp
@@ -6,6 +6,6 @@
 
 void Play_Sound(int index);
 void Display_Splash();
-void Change_Theme(int index, Block* Map [8] [8]);
-void Map_Parser(ifstream &file,Block* Map [8] [8]);
+void Change_Theme(int index, Block* Map [15] [15]);
+void Map_Parser(ifstream &file,Block* Map [15] [15], int levelNum);
 #endif /* Functions_hpp */
